0967f98b934d3d5ccf0d37f7144c09a95b980385
srinisanth93 <srinisanthm.19it@kongu.edu>
2022-06-23 20:57:45 +0530

    Updates

---
+++ precache-manifest.d8a749cd94fdaa458cc5dd0931c5af3b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb0780dfc58d2c4610b952d119b4b6bb",
+    "url": "/AYAPPA_AUTOS/index.html"
+  },
+  {
+    "revision": "92e60d0b04cf3d4c28ff",
+    "url": "/AYAPPA_AUTOS/static/css/main.5dd3d059.chunk.css"
+  },
+  {
+    "revision": "6c4b71171fcb2d4dec33",
+    "url": "/AYAPPA_AUTOS/static/js/2.620040f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/AYAPPA_AUTOS/static/js/2.620040f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92e60d0b04cf3d4c28ff",
+    "url": "/AYAPPA_AUTOS/static/js/main.9e62cc6f.chunk.js"
+  },
+  {
+    "revision": "b41137b84dc932fbe772",
+    "url": "/AYAPPA_AUTOS/static/js/runtime-main.4460c9c4.js"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/AYAPPA_AUTOS/static/media/cart.8bf1af37.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/AYAPPA_AUTOS/static/media/close.2c5552a1.svg"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/AYAPPA_AUTOS/static/media/menu.b3f49afd.svg"
+  },
+  {
+    "revision": "1e6d44624ca3cf28247c14ea8ce8ca44",
+    "url": "/AYAPPA_AUTOS/static/media/undraw_shopping.1e6d4462.svg"
+  },
+  {
+    "revision": "605dac8695d387109eeb186a39d19932",
+    "url": "/AYAPPA_AUTOS/static/media/undraw_vehicle.605dac86.svg"
+  }
+]);
